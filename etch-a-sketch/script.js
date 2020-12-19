efb9b20cc7d6781c7b089936c5faccd707688693
Thi Le <thilehy7865@gmail.com>
2020-12-19 21:18:07 +0700

    Redo changes :)

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -22,8 +22,8 @@ let color = document.createElement('button');
 color.textContent = 'Color';
 let chooseColor = document.createElement('input');
 chooseColor.type = 'color';
-chooseColor.value = '#5e7783';
-subColor.appendChild(color).classList.add('color');
+chooseColor.value = 'black';
+subColor.appendChild(color).classList.add('ccolor');
 subColor.appendChild(chooseColor).classList.add('chooseColor');
 
 
