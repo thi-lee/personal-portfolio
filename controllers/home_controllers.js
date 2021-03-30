6c14ccf8cea28d59c86f0151ad1f327b9c81d55c
Thi Le <thilehy7865@gmail.com>
2021-03-30 16:33:44 +0700

    make it functional

--- controllers/home_controllers.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-    const main = "home/home_main"
-    res.render("index", {main: main});
-});
-
-module.exports = router;
